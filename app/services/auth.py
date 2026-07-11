02f8ef333973c421e97494fc996ea389a7764f67
Anurag-6799 <singhanurag6799@gmail.com>
2026-07-11 23:27:13 +0530

    feat: add JWT access token generation utilities to authentication service

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,17 +1,42 @@
+import jwt
+import os
+from dotenv import load_dotenv
 from passlib.context import CryptContext
 
-# 1. Initialize the hashing context
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+
+# PASSWORD
+# Initialize the hashing context
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# 2. Function to generate a new hash (used during User Registration)
+# Function to generate a new hash (used during User Registration)
 def get_password_hash(password: str) -> str:
     """Takes a plain-text password and returns a secure bcrypt hash."""
     return pwd_context.hash(password)
 
-# 3. Function to compare passwords (used during Login)
+# Function to compare passwords (used during Login)
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """
     Hashes the plain_password and checks if it mathematically matches 
     the hashed_password pulled from the database.
     """
     return pwd_context.verify(plain_password, hashed_password)
+
+
+# TOKEN
+def create_access_token(data: dict) -> str:
+    """Takes a dictionary of user data and encodes it into a JWT."""
+    to_encode = data.copy()
+    
+    # Calculate the expiration time (current UTC time + 60 minutes)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    
+    # Sign the token using the secret key and the HS256 algorithm
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
